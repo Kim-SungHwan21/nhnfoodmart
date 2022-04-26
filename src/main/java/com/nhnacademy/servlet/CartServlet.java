11ab4253c6903e42fcf3581d3ebece7bbdc5ad31
sunghwanKim <shkcreate@naver.com>
2022-04-26 20:47:31 +0900

    nhnmart servelt 수정

--- src/main/java/com/nhnacademy/servlet/CartServlet.java
+++ src/main/java/com/nhnacademy/servlet/CartServlet.java
@@ -15,21 +15,6 @@ public class CartServlet extends HttpServlet {
         resp.setContentType("text/plain");
         resp.setCharacterEncoding("UTF-8");
 
-//        int onionNum = Integer.parseInt(getServletContext().getInitParameter("양파"));
-//        int eggNum = Integer.parseInt(getServletContext().getInitParameter("계란"));
-//        int greenOnionNum = Integer.parseInt(getServletContext().getInitParameter("파"));
-//        int appleNum = Integer.parseInt(getServletContext().getInitParameter("사과"));
-
-//        int onionNum = (int) getServletContext().getAttribute("onionNum");
-//        int eggNum = (int) getServletContext().getAttribute("eggNum");
-//        int greenOnionNum = (int) getServletContext().getAttribute("greenOnionNum");
-//        int appleNum = (int) getServletContext().getAttribute("appleNum");
-
-        String[] putOnionNum = req.getParameterValues("onionNum");
-        String[] putEggNum = req.getParameterValues("eggNum");
-        String[] putGreenOnionNum = req.getParameterValues("greenOnionNum");
-        String[] putAppleNum = req.getParameterValues("appleNum");
-
         String onion = req.getParameter("onionNum");
         String egg = req.getParameter("eggNum");
         String greenOnion = req.getParameter("greenOnionNum");
@@ -39,11 +24,6 @@ public class CartServlet extends HttpServlet {
         int eggNum = egg.isEmpty() ? 0 : Integer.parseInt(egg);
         int greenOnionNum = greenOnion.isEmpty() ? 0 : Integer.parseInt(greenOnion);
         int appleNum = apple.isEmpty() ? 0 : Integer.parseInt(apple);
-
-//        int resultOnionNum = putOnionNum - onionNum;
-//        int resultEggNum =  eggNum;
-//        int resultGreenOnionNum =  greenOnionNum;
-//        int resultAppleNum =  appleNum;
 
         if ((onionNum > 2 && onionNum < 0) || (eggNum > 5 && eggNum < 0 ) || (greenOnionNum > 10 && greenOnionNum < 0) || (appleNum > 20 && appleNum < 0)) {
             resp.setStatus(417);
